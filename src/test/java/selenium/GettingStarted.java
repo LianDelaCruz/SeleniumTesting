8466e36037314f14fa65a35efb741dee12a942f8
Takikato09 <liandelacruz09@hotmail.com>
2025-04-10 15:47:12 +0200

    Testing selenium css selectors

--- src/test/java/selenium/GettingStarted.java
+++ src/test/java/selenium/GettingStarted.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.*;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class GettingStarted {
@@ -16,10 +18,24 @@ public class GettingStarted {
         WebDriver driver = new ChromeDriver(); //uncomment this one
         driver.get("https://www.marshu.com/articles/calculate-addition-calculator-add-two-numbers.php");
         Thread.sleep(5000);  // Let the user actually see something!
-        WebElement first = driver.findElement(By.name("n01"));
-        first.sendKeys("30");
 
-        WebElement second = driver.findElement(By.name("n02"));
+        List<WebElement> elements = driver.findElements(By.className("regfont"));
+
+        int counter = 0;
+        for (WebElement e : elements) {
+            e.sendKeys("1" +counter);
+            counter++;
+        }
+
+
+
+        //searchBox.submit();
+        Thread.sleep(5000);  // Let the user actually see something!
+
+
+        driver.quit();
+
+        /*WebElement second = driver.findElement(By.name("n02"));
         second.sendKeys("20");
 
         WebElement button = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/table[2]/tbody/tr[1]/td[2]/table/tbody/tr/td/div[7]/table/tbody/tr/td/table/tbody/tr/td/form/div[2]/div/div/input"));
@@ -28,12 +44,8 @@ public class GettingStarted {
         WebElement answer = driver.findElement(By.name("answer"));
         String actual = answer.getAttribute("value");
 
-        //searchBox.submit();
-        Thread.sleep(5000);  // Let the user actually see something!
         String expected = "50";
-        assertEquals(expected, actual);
-
-        driver.quit();
+        assertEquals(expected, actual);*/
     }
     //add junit4 to the classpath by hovering on it.
 }
